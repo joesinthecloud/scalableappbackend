3d599b62210b3b677055c4bf2002b4facffcc000
Joe Nervis Jr <jnervjr@Joes-Mac-mini.local>
2026-01-19 21:59:52 -0600

    Add Terraform remote backend scaffold

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project" {
+  type    = string
+  default = "scalableappbackend"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
