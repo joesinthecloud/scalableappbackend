7492cf2faf67b3327503ba881a446e9531b3f3f9
Joe Nervis Jr <jnervjr@gmail.com>
2026-01-20 23:53:32 -0600

    Add ALB request-based autoscaling policy

---
+++ infra/autoscaling-requests.tf
@@ -0,0 +1,19 @@
+resource "aws_appautoscaling_policy" "req_per_target" {
+  name               = "${local.name}-req-per-target"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value = 200.0
+
+    predefined_metric_specification {
+      predefined_metric_type = "ALBRequestCountPerTarget"
+      resource_label         = "${aws_lb.app.arn_suffix}/${aws_lb_target_group.app.arn_suffix}"
+    }
+
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 30
+  }
+}
