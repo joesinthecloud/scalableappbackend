c6ec11b897817c40d4504428aab4323cd466e7d3
Joe Nervis Jr <jnervjr@gmail.com>
2026-01-21 00:12:06 -0600

    Change scaling policy metric to ALBRequestCountPerTarget
    Updated target tracking scaling policy to use ALBRequestCountPerTarget metric.

--- infra/autoscaling.tf
+++ infra/autoscaling.tf
@@ -14,11 +14,13 @@ resource "aws_appautoscaling_policy" "cpu" {
   service_namespace  = aws_appautoscaling_target.ecs.service_namespace
 
   target_tracking_scaling_policy_configuration {
-    target_value = 50.0
+  target_value = 50.0
 
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
+  predefined_metric_specification {
+    predefined_metric_type = "ALBRequestCountPerTarget"
+    resource_label         = "${aws_lb.app.arn_suffix}/${aws_lb_target_group.app.arn_suffix}"
+  }
+}
 
     scale_in_cooldown  = 60
     scale_out_cooldown = 60
